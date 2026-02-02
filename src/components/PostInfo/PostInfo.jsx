ae655c2121641da12c857d339cd85928bc4c9575
Oleksandr Hordiienko <g.aleks2308@gmail.com>
2026-02-02 21:34:13 +0200

    add solution

--- src/components/PostInfo/PostInfo.jsx
+++ src/components/PostInfo/PostInfo.jsx
@@ -1,1 +1,28 @@
-export const PostInfo = () => <>Put the post here</>;
+import commentsFromServer from '../../api/comments.json';
+import { CommentList } from '../CommentList/CommentList';
+
+export const PostInfo = ({ post }) => (
+  <div className="PostInfo">
+    <div className="PostInfo__header">
+      <h3 className="PostInfo__title">{post.title}</h3>
+
+      <p>
+        {' Posted by  '}
+
+        {post.user && (
+          <a className="UserInfo" href={`mailto:${post.user.email}`}>
+            {post.user.name}
+          </a>
+        )}
+      </p>
+    </div>
+
+    <p className="PostInfo__body">{post.body}</p>
+
+    <CommentList
+      comments={commentsFromServer.filter(
+        comment => comment.postId === post.id,
+      )}
+    />
+  </div>
+);
